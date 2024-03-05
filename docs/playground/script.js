590797a6d9cd6818c04786471cd987f4d61c220f
yorunohikari <90948140+yorunohikari@users.noreply.github.com>
2024-03-05 17:34:11 +0700

    fix

--- docs/playground/script.js
+++ docs/playground/script.js
@@ -4,20 +4,36 @@ document.getElementById("messageForm").addEventListener("submit", function (even
   // Get input values
   const yourName = document.getElementById("yourName").value;
   const NIM = document.getElementById("NIM").value;
+  const professorTitleElement = document.querySelector('.radio-options input[name="professorTitle"]:checked');
+  const professorTitle = professorTitleElement ? professorTitleElement.value : '';
   const professorName = document.getElementById("professorName").value;
-  const professorTitle = document.getElementById("professorTitle").value;
   const courseDetails = document.getElementById("courseDetails").value;
   const absenceDates = document.getElementById("absenceDates").value;
   const situation = document.getElementById("situation").value;
-  
 
   // Generate message
-  const message = `Assalamualaikum warahmatullahi wabarakatuh
+  let message = `Assalamualaikum warahmatullahi wabarakatuh
 
-Mohon maaf mengganggu waktu ${professorTitle} ${professorName}.
+Mohon maaf mengganggu waktu `;
+  
+  if (professorTitle === 'Sensei') {
+    message += `${professorName} ${professorTitle}`;
+  } else {
+    message += `${professorTitle} ${professorName}`;
+  }
+  
+  message += `.
 
 Saya ${yourName}, NIM ${NIM}, kelas ${courseDetails}. Melalui pesan ini, saya memberitahukan saat ini saya sedang ${situation}. Sehingga saya tidak bisa mengikuti kelas pada hari ${absenceDates}. 
-Untuk itu, saya mohon pengertian dari ${professorTitle} ${professorName} agar memberi izin saya untuk tidak mengikuti kelas. Demikian yang dapat saya sampaikan. Mohon maaf apabila ada kata-kata yang kurang berkenan. 
+Untuk itu, saya mohon pengertian dari `;
+
+  if (professorTitle === 'Sensei') {
+    message += `${professorName} ${professorTitle}`;
+  } else {
+    message += `${professorTitle} ${professorName}`;
+  }
+
+  message += ` agar memberi izin saya untuk tidak mengikuti kelas. Demikian yang dapat saya sampaikan. Mohon maaf apabila ada kata-kata yang kurang berkenan. 
 
 Terima kasih 🙏🏻`;
 
@@ -25,7 +41,8 @@ Terima kasih 🙏🏻`;
   document.getElementById("outputMessage").value = message;
 });
 
-document.getElementById("copyButton").addEventListener("click", function() {
+
+document.getElementById("copyButton").addEventListener("click", function () {
   // Select the text inside the textarea
   const outputMessage = document.getElementById("outputMessage");
   outputMessage.select();
