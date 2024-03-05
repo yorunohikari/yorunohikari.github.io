c39ca3049dab257a14dd055474e5d1539e88aeba
yorunohikari <90948140+yorunohikari@users.noreply.github.com>
2024-03-05 18:24:36 +0700

    adding salam and shit

--- docs/playground/script.js
+++ docs/playground/script.js
@@ -10,12 +10,57 @@ document.getElementById("messageForm").addEventListener("submit", function (even
   const courseDetails = document.getElementById("courseDetails").value;
   const absenceDates = document.getElementById("absenceDates").value;
   const situation = document.getElementById("situation").value;
+  // Get the selected value from the dropdown menu
+  const salamDropdown = document.getElementById('salam');
+  const selectedSalam = salamDropdown.value;
+
+  // Parse the date string
+  const date = new Date(absenceDates);
+
+  // Define an array of Indonesian day names
+  const dayNames = [
+    "Minggu",
+    "Senin",
+    "Selasa",
+    "Rabu",
+    "Kamis",
+    "Jumat",
+    "Sabtu"
+  ];
+
+  // Define an array of Indonesian month names
+  const monthNames = [
+    "Januari",
+    "Februari",
+    "Maret",
+    "April",
+    "Mei",
+    "Juni",
+    "Juli",
+    "Agustus",
+    "September",
+    "Oktober",
+    "November",
+    "Desember"
+  ];
+
+  // Format the date
+  const formattedDate = `${dayNames[date.getDay()]}, ${date.getDate()} ${monthNames[date.getMonth()]} ${date.getFullYear()}`;
+
 
   // Generate message
-  let message = `Assalamualaikum warahmatullahi wabarakatuh
+  let message = '';
+
+  if (selectedSalam === 'Aww') {
+    message += `Assalamualaikum Warahmatullahi Wabarakatuh
 
 Mohon maaf mengganggu waktu `;
-  
+  } else {
+    message += `Selamat ${selectedSalam.toLowerCase()}
+
+Mohon maaf mengganggu waktu `;
+  }
+
   if (professorTitle === 'Sensei') {
     message += `${professorName} ${professorTitle}`;
   } else if (professorTitle === 'Prof') {
@@ -23,14 +68,16 @@ Mohon maaf mengganggu waktu `;
   } else {
     message += `${professorTitle} ${professorName}`;
   }
-  
+
   message += `.
 
-Saya ${yourName}, NIM ${NIM}, kelas ${courseDetails}. Melalui pesan ini, saya memberitahukan saat ini saya sedang ${situation}. Sehingga saya tidak bisa mengikuti kelas pada hari ${absenceDates}. 
+Saya ${yourName}, NIM ${NIM}, kelas ${courseDetails}. Melalui pesan ini, saya memberitahukan saat ini saya sedang ${situation}. Sehingga saya tidak bisa mengikuti kelas pada hari ${formattedDate}. 
 Untuk itu, saya mohon pengertian dari `;
 
   if (professorTitle === 'Sensei') {
     message += `${professorName} ${professorTitle}`;
+  } else if (professorTitle === 'Prof') {
+    message += `${professorTitle} ${professorName}`;
   } else {
     message += `${professorTitle} ${professorName}`;
   }
