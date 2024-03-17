c19ecfe0d8de392bfad88c6f487e8774654f5bbe
yorunohikari <90948140+yorunohikari@users.noreply.github.com>
2024-03-17 20:18:38 +0700

    finalizing neko

---
+++ docs/playground/sw.js
@@ -0,0 +1,21 @@
+
+// Change this to your repository name
+var GHPATH = '/projects/neko-kanji';
+
+// Choose a different app prefix name
+var APP_PREFIX = 'nkj_';
+
+// The version of the cache. Every time you change any of the files
+// you need to change this version (version_01, version_02…). 
+// If you don't change the version, the service worker will give your
+// users the old files!
+var VERSION = 'version_04';
+
+// The files to make available for offline use. make sure to add 
+// others to this list
+var URLS = [    
+  `${GHPATH}/`,
+  `${GHPATH}/index.html`,
+  `${GHPATH}/style.css`,
+  `${GHPATH}/script.js`
+]
