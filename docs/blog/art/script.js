3ffb97dfb7da87884ddbd2914427ddcd1e857adb
yorunohikari <90948140+yorunohikari@users.noreply.github.com>
2024-03-19 18:30:10 +0700

    big change

--- docs/blog/art/script.js
+++
@@ -1,5 +0,0 @@
-// Get the current year
-var currentYear = new Date().getFullYear();
-
-// Set the copyright information dynamically
-document.getElementById("copyright").innerHTML = "Copyrights &#169; " + currentYear + " Osha Permadi. All Rights Reserved.";
